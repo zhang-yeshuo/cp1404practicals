5bd52ebec7b855ad169857cfac43c3c451db3829
zhang-yeshuo <yeshuo.zhang@jcu.edu.au>
2024-10-09 14:53:22 +0800

    Add prac 4 files

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,25 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data()
-    print(data)
+    subjects = load_data()
+    display_subject_details(subjects)
+
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    subjects = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        line = line.strip()
+        parts = line.split(',')
+        parts[2] = int(parts[2])
+        subjects.append(parts)
     input_file.close()
+    return subjects
 
+def display_subject_details(subjects):
+    for subject in subjects:
+        print(f"{subject[0]} is taught by {subject[1]} and has {subject[2]} students")
 
 main()
