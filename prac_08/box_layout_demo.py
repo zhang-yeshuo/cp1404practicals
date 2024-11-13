b25d106676e0bc064c779857cd86be225b5b0342
zhang-yeshuo <yeshuo.zhang@jcu.edu.au>
2024-11-13 20:44:26 +0800

    Complete prac_08 box_layout

---
+++ prac_08/box_layout_demo.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class BoxLayoutDemo(App):
+    def build(self):
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+    def handle_greet(self):
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+    def handle_clear(self):
+        self.root.ids.input_name.text = ''
+
+BoxLayoutDemo().run()
