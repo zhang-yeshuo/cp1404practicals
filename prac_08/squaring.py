d330d89d8932b8eee667677fe07bb61d11ae23bf
zhang-yeshuo <yeshuo.zhang@jcu.edu.au>
2024-11-17 15:02:11 +0800

    Complete prac_08 squaring

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -1,16 +1,8 @@
-"""
-CP1404/CP5632 Practical
-Kivy GUI program to square a number
-Lindsay Ward, IT@JCU
-Started 13/10/2015
-"""
+
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
-
-__author__ = 'Lindsay Ward'
-
 
 class SquareNumberApp(App):
     """ SquareNumberApp is a Kivy App for squaring a number """
